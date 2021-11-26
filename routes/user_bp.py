fe31aaa244f3109060e6b9a9caadc764687bab67
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-11-26 18:15:04 -0300

    esqueci de colocar a rota bobo

--- routes/user_bp.py
+++ routes/user_bp.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from controllers.UserController import index, store, show, update, delete
+from controllers.UserController import index, store, show, update, delete, logout
 
 user_bp = Blueprint('user_bp', __name__)
 user_bp.route('/', methods=['GET'])(index)
@@ -8,3 +8,4 @@ user_bp.route('/create', methods=['POST'])(store)
 user_bp.route('/<int:user_id>', methods=['GET'])(show)
 user_bp.route('/<int:user_id>/edit', methods=['POST'])(update)
 user_bp.route('/<int:user_id>', methods=['DELETE'])(delete)
+user_bp.route('/logout', methods=['GET'])(logout)
