3dff8547196d8467dc4365dcb5a482ad1676d450
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-11-26 20:37:49 -0300

    corrigindo controllers

--- routes/notice_bp.py
+++ routes/notice_bp.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-from controllers.UserController import index, store, show, update, delete, logout
+from controllers.NoticeController import index, store, show, update, delete, logout
 
 notice_bp = Blueprint('notice_bp', __name__)
 notice_bp.route('/create', methods=['POST', 'GET'])(store)
