ffc42b5e6e2ed54bfcac4afbe761503f3c25ec01
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-11-25 21:31:39 -0300

    alterando configurações

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template
 from flask_migrate import Migrate
 from models import db
 from routes.user_bp import user_bp
+
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db.init_app(app)
 migrate = Migrate(app, db)
 app.register_blueprint(user_bp, url_prefix='/users')
