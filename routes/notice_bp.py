4db48d487134ce4fbba3d9d8cc33d99765a25611
DyogoBendo <dyogoromagnabendo@gmail.com>
2021-12-13 12:33:15 -0300

    corrigindo rota

--- routes/notice_bp.py
+++ routes/notice_bp.py
@@ -5,4 +5,4 @@ from controllers.NoticeController import store, show, delete
 notice_bp = Blueprint('notice_bp', __name__)
 notice_bp.route('/create', methods=['POST', 'GET'])(store)
 notice_bp.route('/<int:notice_id>', methods=['GET'])(show)
-notice_bp.route('/<int:notice_id>', methods=['GET'])(delete)
+notice_bp.route('/delete/<int:notice_id>', methods=['GET'])(delete)
